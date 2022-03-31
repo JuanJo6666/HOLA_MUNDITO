93940d94311c6cd65101207ed6f469943557b198
Juan <102483340+JuanJo6666@users.noreply.github.com>
2022-03-31 16:31:20 -0500

    add sysos

--- src/HOLA.java
+++ src/HOLA.java
@@ -1,6 +1,7 @@
 public class HOLA {
     public static void main(String[] args) {
-        System.out.println("Hola,ya instale java");
+        System.out.println("Hola, ya subi mi codigo a GIT");
+        System.out.println("Soy Pro");
     }
 
 }
