47b09fe2a33b724d334af3c3b5f2961b1ff88ae7
Juan <102483340+JuanJo6666@users.noreply.github.com>
2022-03-31 16:37:13 -0500

    efue
    efue

--- src/HOLA.java
+++ src/HOLA.java
@@ -2,6 +2,7 @@ public class HOLA {
     public static void main(String[] args) {
         System.out.println("Hola, ya subi mi codigo a GIT");
         System.out.println("Soy Pro");
+        System.out.println("-_-");
     }
 
 }
