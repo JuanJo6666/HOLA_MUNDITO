59b2b75bde6b0827c4b787ab98229563f9b1bbad
Juan <102483340+JuanJo6666@users.noreply.github.com>
2022-03-31 16:47:41 -0500

    update

--- src/HOLA.java
+++ src/HOLA.java
@@ -1,9 +1,6 @@
 public class HOLA {
     public static void main(String[] args) {
         System.out.println("Hola, ya subi mi codigo a GIT");
-        System.out.println("Soy Pro");
-        System.out.println("-_-");
-        System.out.println(":v");
     }
 
 }
